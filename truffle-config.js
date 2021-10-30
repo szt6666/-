bc8262510984e2df49dab8e8e4da0cf0fd398992
szt6666 <91237445+szt6666@users.noreply.github.com>
2021-10-30 12:43:16 +0800

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,18 @@
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+var mnemonic = "radar they uphold three awful merry license flee detail mobile hobby upper";
+
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    }
+  }
+};
